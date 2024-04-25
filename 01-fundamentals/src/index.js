b09866683e660efa663d4c6804d33740e82c7215
run4theh111z <130161929+run4theh111z@users.noreply.github.com>
2024-04-24 22:05:46 -0400

    watever

--- 01-fundamentals/src/index.js
+++ 01-fundamentals/src/index.js
@@ -5,37 +5,45 @@ import './index.css'
 
 const books = [
   {
+    id: 1,
     name: 'Fight Club',
     author: 'Chuck Palahniuk',
     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUQHrDSARiAi40mnBrXnU9o1nm4hygOLUAN5TNqFqIVA&s',
   },
   {
+    id: 2,
     name: 'The Alchemist',
     author: 'Paulo Cuelo',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8s7ZQ7z1tjJ8BzE8H3YRv2y8s6H6JQ8Zk3y1h2c2q2w&s',
+    img: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmiro.medium.com%2Fmax%2F552%2F0*oGUMQVMVxjXCcS1B.jpg&f=1&nofb=1&ipt=368c38fca20864b9ca0ea49f37b821dfb18e799924a152b6d6fe58cc03ceadce&ipo=images',
   }
 ]
+
 const BookList = () => {
   return (
     <section className='booklist'>
- 
+      {books.map((book) => {
+        const { name, author, img, id } = book;
+        return <Book key={id} name={name} author={author} img={img} />
+      })}
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, name, author } = props;
+  
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <Image img={img} />
+      <Title name={name} />
+      <Author author={author} />
     </article>
   )
 }
 
-const Image = () => <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUQHrDSARiAi40mnBrXnU9o1nm4hygOLUAN5TNqFqIVA&s" alt="fight-club-book-cover" />
-const Title = () => <h2>Fight Club</h2>
-const Author = () => <h4>Chuck P.</h4>
+const Image = ({ img }) => <img src={img} alt="fight-club-book-cover" />
+const Title = ({ name }) => <h2>{name}</h2>
+const Author = ({ author }) => <h4>{author}</h4>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
