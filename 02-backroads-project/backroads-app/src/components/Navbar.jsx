d27a7e3dc53cc3f45744b83a617613e5d5d68146
Justin Gardner <justin_gardner@ncci.com>
2024-06-03 17:30:56 -0400

    social links

--- 02-backroads-project/backroads-app/src/components/Navbar.jsx
+++ 02-backroads-project/backroads-app/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import logo from '../images/logo.svg';
 import { pageLinks } from '../data';
+import { socialLinks } from '../data';
 
 const Navbar = () => {
   return (
@@ -26,21 +27,16 @@ const Navbar = () => {
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-            ><i className="fab fa-facebook"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-            ><i className="fab fa-twitter"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="nav-icon"
-            ><i className="fab fa-squarespace"></i
-            ></a>
-          </li>
+          {socialLinks.map((link) => {
+            const { id, href, icon } = link;
+            return (
+              <li key={id}>
+                <a href={href} target="_blank" rel="noreferrer" className="nav-icon">
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
