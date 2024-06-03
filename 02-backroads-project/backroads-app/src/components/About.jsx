db7ed85fb50bde9610d019a00761944d45d01450
Justin Gardner <justin_gardner@ncci.com>
2024-06-03 17:35:38 -0400

    hero and about

--- 02-backroads-project/backroads-app/src/components/About.jsx
+++ 02-backroads-project/backroads-app/src/components/About.jsx
@@ -1,4 +1,4 @@
-import about from '../images/about.jpeg'
+import aboutImg from '../images/about.jpeg'
 
 const About = () => {
   return (
@@ -10,7 +10,7 @@ const About = () => {
       <div className="section-center about-center">
         <div className="about-img">
           <img
-            src={about}
+            src={aboutImg}
             className="about-photo"
             alt="awesome beach"
           />
