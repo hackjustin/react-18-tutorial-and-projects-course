d9514c06f30056a782eeb2ceeecb1af751cf5e02
Horns Capital <102259246+hornscapital@users.noreply.github.com>
2024-06-13 21:04:53 -0400

    usestate 1

--- 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,15 @@
+import React, { useState } from 'react';
+
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h2>useState error example</h2>;
+      <h1>Count: {count}</h1>
+      <button className="btn" onClick={() => setCount(count + 1)}>Increase</button>
+    </>
+  )
 };
 
 export default ErrorExample;
